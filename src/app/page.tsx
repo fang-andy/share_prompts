64b0d9855609a3f3de756daf60a09a83c9e6b690
fang-andy <afang1094@gmail.com>
2023-06-05 15:35:12 -0700

    committing initial files

---
+++ src/app/page.tsx
@@ -0,0 +1,20 @@
+import { Inter } from 'next/font/google'
+
+import type { Metadata } from 'next'
+import LargeHeading from '../components/ui/LargeHeading'
+
+export const metadata: Metadata = {
+    title: 'Similarity API | Home',
+    description: 'Free & open-source text similarity API',
+}
+export default function Home() {
+    return (
+        <div className='relative h-screen flex items-center justify-center overflow-x-hidden'>
+            <div className='container pt-32 max-w-7xl mx-auto w-full h-full'>
+                <div className='h-full gap-6 flex flex-col justify-start lg:justify-center items-center lg:items-start'>
+                    <LargeHeading size='lg' className='three-d text-black dark:text-light-gold'></LargeHeading>
+                </div>
+            </div>
+        </div>
+    )
+}
